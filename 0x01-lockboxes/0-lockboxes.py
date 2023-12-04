e18106b3e01269061cea3987ca6065b74f7fa36a
CARTOON <brianngugi224@gmail.com>
2023-12-04 12:18:54 +0300

    functions

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''
+    0x01. Lockboxes
+'''
+
+def canUnlockAll(boxes)
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or
